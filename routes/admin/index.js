ffe02df68351d1b71eca8fa7ba4dd4de0bd8a85c
Martin Cornelio Piscitelli <mcp314@gmail.com>
2023-04-13 00:04:07 -0300

    Modulo 5  unidad 4

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/admin/login')
+  res.render('/admin/novedades',{
+    layout: 'admin/layout',
+    usuario: req.session.nombre,
+  });
 });
 
 module.exports = router;
